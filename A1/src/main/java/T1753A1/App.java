33e8bac976a4946f35afab727ac03199fd0958e4
Todd Gaunt <toddgaunt@protonmail.ch>
2018-09-03 19:37:45 -0400

    Implement writing and searching of index

--- A1/src/main/java/T1753A1/App.java
+++ A1/src/main/java/T1753A1/App.java
@@ -1,6 +1,29 @@
 package T1753A1;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Path;
+
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.CompositeReader;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+import edu.unh.cs.treccar_v2.Data;
+import edu.unh.cs.treccar_v2.read_data.DeserializeData;
 
 /**
  * Hello world!
@@ -10,6 +33,36 @@ public class App
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		try {
+			//if (args.length != 2) {
+			//	System.out.println("Usage: A1 <document to index>");
+			//	return;
+			//}
+			//String dataFile = args[1];
+			String dataFile = "data/test200/test200-train/fold-0-train.pages.cbor-paragraphs.cbor";
+			/* Setup the indexer */
+			
+			Directory indexDir = FSDirectory.open(new File("index").toPath());
+			IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+			IndexWriter iwriter = new IndexWriter(indexDir, config);
+			
+	
+			/* Deserialize the data and add documents to the index */
+			FileInputStream fp = new FileInputStream(dataFile);
+			for (Data.Paragraph para : DeserializeData.iterableParagraphs(fp)) {
+				Document doc = new Document();
+				doc.add(new StringField("id", para.getParaId(), Field.Store.YES));
+				doc.add(new TextField("text", para.getTextOnly(), Field.Store.YES));
+				iwriter.addDocument(doc);
+			}
+			iwriter.close();
+			System.out.println("foo");
+			/* Use the index */
+			IndexSearcher is = new IndexSearcher(DirectoryReader.open(indexDir));
+			QueryParser parser = new QueryParser("content", new StandardAnalyzer());
+			System.out.println("bar");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
