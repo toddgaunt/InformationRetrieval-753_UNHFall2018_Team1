babf4280024bd3e6a8e8b8bfffa6ff8b37662175
Todd Gaunt <toddgaunt@protonmail.ch>
2018-09-03 01:21:13 -0400

    Exit A1/setup.sh early if dependencies are not present

--- A1/setup.sh
+++ A1/setup.sh
@@ -1,5 +1,13 @@
 # Script to run when first setting this maven project on a new computer
 
+if [ ! -e ../lib/lucene-7.4.0/core/lucene-core-7.4.0.jar ] \
+|| [ ! -e ../lib/trec-car-tools-java/target/trec-car-tools-java-15.jar ]
+then
+	echo "Please run ../setup.sh to download dependencies before"
+	echo "installing them with this script"
+	exit 1
+fi
+
 mvn install:install-file \
 	-Dfile=../lib/lucene-7.4.0/core/lucene-core-7.4.0.jar \
 	-DgroupId=org.apache.lucene \
