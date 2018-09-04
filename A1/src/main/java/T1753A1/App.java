0a80b2742ddf73d979f79c65ae4fee950cac705e
Andrew Lewis <lewisandrew425@gmail.com>
2018-09-03 20:34:53 -0400

    Add second and third query

--- A1/src/main/java/T1753A1/App.java
+++ A1/src/main/java/T1753A1/App.java
@@ -20,6 +20,7 @@ import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.Collector;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -57,7 +58,8 @@ public class App
 			ScoreDoc[] hits;
 			
 			/* Perform the first query */
-			results = is.search(parser.parse("text: power text: nap text: benefits"), 10);
+			Query query1 = parser.parse("text: power text: nap text: benefits");
+			results = is.search(query1, 10);
 			hits = results.scoreDocs;
 			System.out.println("Begin Q1: power nap benefits");
 			for (ScoreDoc hit: hits) {
@@ -65,9 +67,28 @@ public class App
 				System.out.println(doc.get("id"));
 			}
 			System.out.println("End Q1");
-			/* TODO(todd): Perform the second query */
 			
-			/* TODO(todd): Perform third query */
+			/* Perform the second query */
+			Query query2 = parser.parse("text: whale text: vocalization text: production text: of text: sound");
+			results = is.search(query2, 10);
+			hits = results.scoreDocs;
+			System.out.println("Begin Q2: whale vocalization production of sound");
+			for (ScoreDoc hit: hits) {
+				Document doc = is.doc(hit.doc);
+				System.out.println(doc.get("id"));
+			}
+			System.out.println("End Q2");
+
+			/* Perform the third query */
+			Query query3 = parser.parse("text: pokemon text: puzzle text: league");
+			results = is.search(query3, 10);
+			hits = results.scoreDocs;
+			System.out.println("Begin Q3: pokemon puzzle league");
+			for (ScoreDoc hit: hits) {
+				Document doc = is.doc(hit.doc);
+				System.out.println(doc.get("id"));
+			}
+			System.out.println("End Q3");
 		} catch (Exception e) {
 			System.out.println(e);
 		}
