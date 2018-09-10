a10867da5be807df941c7f4ff83bc895f7ed5288
Todd Gaunt <toddgaunt@protonmail.ch>
2018-09-10 13:01:14 -0400

    Setup second assignment directory

---
+++ A2/src/main/java/cs753/T1/A2/App.java
@@ -0,0 +1,13 @@
+package cs753.T1.A2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
