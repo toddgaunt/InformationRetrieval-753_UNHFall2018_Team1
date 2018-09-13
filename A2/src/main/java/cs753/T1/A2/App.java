e527068784db59458f920d5fd52325457791d3cb
Todd Gaunt <toddgaunt@protonmail.ch>
2018-09-12 23:43:06 -0400

    Add usage message and file opening

--- A2/src/main/java/cs753/T1/A2/App.java
+++ A2/src/main/java/cs753/T1/A2/App.java
@@ -1,13 +1,36 @@
 package cs753.T1.A2;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Path;
+
+import edu.unh.cs.treccar_v2.Data;
+import edu.unh.cs.treccar_v2.read_data.DeserializeData;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	public static void usage()
+	{
+		System.out.println("usage: A2 file");
+		System.exit(-1);
+	}
+
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+		String dataFile;
+
+		if (args.length != 1)
+			usage();
+		dataFile = args[0];
+		try {
+			FileInputStream fp = new FileInputStream(dataFile);
+		} catch (Exception e) {
+			System.out.println("Please specify an input file that exists");
+		}
     }
 }
