e5cfebca0e39692998687bb8c5d17e75e1af8afb
Todd Gaunt <toddgaunt@protonmail.ch>
2018-09-16 17:39:43 -0400

    Implement A2 part 1 halfway

--- A2/src/main/java/cs753/T1/A2/App.java
+++ A2/src/main/java/cs753/T1/A2/App.java
@@ -2,6 +2,7 @@ package cs753.T1.A2;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.PrintWriter;
 import java.util.List;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -16,81 +17,108 @@ import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.BasicStats;
+import org.apache.lucene.search.similarities.SimilarityBase;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 import edu.unh.cs.treccar_v2.Data;
 import edu.unh.cs.treccar_v2.read_data.DeserializeData;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	public static void usage()
 	{
-		System.out.println("usage: A2 file");
+		System.out.println("usage: make run PARA=path/to/paragraphs OUTLINE=path/to/outlines METHOD=<default|custom>");
 		System.exit(-1);
 	}
 	
-	public static void printQuery(IndexSearcher is, String query) {
-		try {
+	public static String getQueryRFF(IndexSearcher is, String pageID, String query, int method) throws Exception {
+		int rank = 1;
+		String ret = "";
+		SimilarityBase customSim = new SimilarityBase() {;
+			protected float score(BasicStats stats, float freq, float doclen) {
+				return 0.0f;
+			}
+
+			@Override
+			public String toString() {
+				return "Custom";
+			}
+		};
+
 		QueryParser parser = new QueryParser("content", new StandardAnalyzer());
 		TopDocs results;
 		ScoreDoc[] hits;
 		
-		/* Perform the first query */
-		results = is.search(parser.parse(query), 10);
+		if (1 == method) {
+			is.setSimilarity(customSim);
+		}
+		
+		results = is.search(parser.parse(query), 100);
 		hits = results.scoreDocs;
-		System.out.println("Begin: '" + query + "'");
 		for (ScoreDoc hit: hits) {
 			Document doc = is.doc(hit.doc);
-			System.out.println(doc.get("id"));
-			System.out.println(doc.get("text"));
+			ret += pageID;
+			ret += " " + doc.get("id");
+			ret += " " + rank;
+			ret += " " + hit.score;
+			if (1 == method) {
+				ret += " team1-custom";
+			} else {
+				ret += " team1-default";
+			}
+			ret += "\n";
+			rank += 1;
 		}
-		System.out.println("End '" + query +"'");
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+		return ret;
 	}
 
     public static void main(String[] args)
     {
     	try {
 			String dataFile;
-			String queryFile;
-			FileInputStream fp;
+			String outlineFile;
+			String methodName;
+			int method = 0;
 
-			if (args.length != 2)
+			if (args.length != 3)
 				usage();
 			dataFile = args[0];
-			queryFile = args[1];
+			outlineFile = args[1];
+			methodName = args[2];
+			if (methodName.equals("default")) {
+				method = 0;
+			} else if (methodName.equals("custom")) {
+				method = 1;
+			} else {
+				usage();
+			}
 			
 			/* Setup the indexer */
 			Directory indexDir = FSDirectory.open(new File("index").toPath());
 			IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
 			IndexWriter iwriter = new IndexWriter(indexDir, config);
-			
 	
 			/* Deserialize the data and add documents to the index */
-			fp = new FileInputStream(dataFile);
-			for (Data.Paragraph para : DeserializeData.iterableParagraphs(fp)) {
+			FileInputStream fp_para = new FileInputStream(dataFile);
+			for (Data.Paragraph para : DeserializeData.iterableParagraphs(fp_para)) {
 				Document doc = new Document();
 				doc.add(new StringField("id", para.getParaId(), Field.Store.YES));
 				doc.add(new TextField("text", para.getTextOnly(), Field.Store.YES));
 				iwriter.addDocument(doc);
 			}
 			iwriter.close();
-			fp.close();
 			/* Use the index */
 			IndexSearcher is = new IndexSearcher(DirectoryReader.open(FSDirectory.open(new File("index").toPath())));
 			
-			fp = new FileInputStream(queryFile);
-			for (Data.Page page : DeserializeData.iterableAnnotations(fp)) {
-				System.out.println(page.getPageId());
+			PrintWriter outfile = new PrintWriter(methodName + ".txt", "UTF-8");
+			FileInputStream fp_outline = new FileInputStream(outlineFile);
+			for (Data.Page page : DeserializeData.iterableAnnotations(fp_outline)) {
+				outfile.print(getQueryRFF(is, page.getPageId(), "text: " + page.getPageName(), method));
 			}
-			fp.close();
+			outfile.println();
+			fp_outline.close();
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
