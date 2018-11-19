24db2f4eecb19de819765488a4974260ce762844
Francisco Santana <f.santana31@outlook.com>
2018-11-19 18:37:40 -0500

    Finish up Q2 function

--- A5/src/main/java/cs753/T1/A5/App.java
+++ A5/src/main/java/cs753/T1/A5/App.java
@@ -5,17 +5,16 @@ import java.io.FileInputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.*;
 //import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.ScoreDoc;
@@ -40,32 +39,35 @@ public class App
 		System.exit(-1);
 	}
 
-	public static ArrayList<Rank> getQueryRFF(IndexSearcher is, String pageID, String query, Similarity method) throws Exception {
-        int rank = 1;
-        String ret = "";
-	    ArrayList<Rank> ranks = new ArrayList<Rank>();
+	public static Rank[] getQueryRFF(IndexSearcher is, String query, Similarity method) throws Exception {
+        int rank = 0;
+	    Rank[] ranks = new Rank[10];
 
 		QueryParser parser = new QueryParser("content", new StandardAnalyzer());
 		TopDocs results;
 		ScoreDoc[] hits;
 
+
 		is.setSimilarity(method);
 
-		results = is.search(parser.parse(query), 100);
+		results = is.search(parser.parse(query), 10);
 		hits = results.scoreDocs;
 		for (ScoreDoc hit: hits) {
 			Document doc = is.doc(hit.doc);
-			ranks.add(new Rank(doc.get("id"), 0));
+			ranks[rank] = new Rank(doc.get("id"), 0);
+			rank +=1;
 		}
 		return ranks;
 	}
 
 	
 	public static void question2(String[] args) throws Exception {
-		String dataFile;
-		String outline;
-		String methods[] = new String[]{"lnc.ltn", "bnn.bnn", "LM-U", "U-JM", "U-DS"};
-		ArrayList<ArrayList<Rank>> listOfRanks = new ArrayList<ArrayList<Rank>>();
+	    String dataFile;
+        String outline;
+        int queryID = 0;
+		Rank[][] listOfRanks = new Rank[5][10];
+        TFIDF tfidf = new TFIDF();
+        LM lm = new LM();
 
 		if (args.length != 3)
 			usage();
@@ -90,14 +92,30 @@ public class App
 		/* Use the index */
 		IndexReader ir = DirectoryReader.open(FSDirectory.open(new File("index").toPath()));
 		IndexSearcher is = new IndexSearcher(ir);
-		
-		PrintWriter outfile = new PrintWriter("out.runfile", "UTF-8");
-		FileInputStream fp_outline = new FileInputStream(outline);
-		for (Data.Page page : DeserializeData.iterableAnnotations(fp_outline)) {
-			outfile.print(getQueryRFF(is, page.getPageId(), "text: " + page.getPageName(), method));
-		}
-		fp_outline.close();
-		outfile.close();
+
+        Set<String> s = new HashSet<String>();
+        for (int i = 0; i < ir.numDocs(); i++) {
+            for (IndexableField j: ir.document(i).getFields())
+            {
+                s.add(j.name());
+            }
+        }
+        number_of_terms = s.size();
+
+
+        PrintWriter outfile = new PrintWriter("out.runfile", "UTF-8");
+        FileInputStream fp_outline = new FileInputStream(outline);
+        for (Data.Page page : DeserializeData.iterableAnnotations(fp_outline)) {
+            listOfRanks[0] = getQueryRFF(is, "text: " + page.getPageName(), tfidf.LncLtnSim());
+            listOfRanks[1] = getQueryRFF(is, "text: " + page.getPageName(), tfidf.BnnBnnSim());
+            listOfRanks[2] = getQueryRFF(is, "text: " + page.getPageName(), lm.U_L(number_of_terms));
+            listOfRanks[3] = getQueryRFF(is, "text: " + page.getPageName(), lm.U_JM());
+            listOfRanks[4] = getQueryRFF(is, "text: " + page.getPageName(), lm.U_DS());
+            outfile.print(Rank.rankLibFmtFromRanklist(queryID, listOfRanks));
+            queryID+=1;
+        }
+        fp_outline.close();
+        outfile.close();
 	}
 	
 	public static void main(String[] args)
